f20c71b413b34218bc6f6502392fdda89f166813
Anfal <Anfal.alatawi@gmail.com>
2019-12-22 03:50:38 +0300

    first commit

---
+++ venv/calculate_age.py
@@ -0,0 +1,17 @@
+import datetime
+
+
+# Get the user's birthday
+myDay = input("Please enter the Day you were born: ")
+myMonth = input("Please enter the Month you were born: ")
+myYear = input("Please enter the Year you were born: ")
+
+print("Your birthday is on "+str(myDay)+"-"+str(myMonth)+"-"+str(myYear))
+
+# Calculating the user's age
+myBirthday = datetime.date(int(myYear), int(myMonth), int(myDay))
+today = datetime.date.today()
+dayDiff = (today-myBirthday)
+
+# Print the user's age
+print("Your age is "+str(int(dayDiff.days/365.25))+" Years")
