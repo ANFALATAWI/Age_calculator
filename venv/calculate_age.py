ce17203ec52cca9e853a992e13af315d9d40cc75
Anfal <Anfal.alatawi@gmail.com>
2019-12-26 04:47:29 +0300

    Fixed input error

--- venv/calculate_age.py
+++ venv/calculate_age.py
@@ -2,20 +2,25 @@ import datetime
 
 # Print headers:
 print("---------------------------------------------------")
-print("Age Calulcator veriosn 1.0")
+print("Age Calulcator veriosn 2.0")
 print("---------------------------------------------------")
 
-# Get the user's birthday
-myDay = input("Please enter the Day you were born: ")
-myMonth = input("Please enter the Month you were born: ")
-myYear = input("Please enter the Year you were born: ")
+while True:
+    try:
+        # Get the user's birthday
+        myDay = input("Please enter the Day you were born: ")
+        myMonth = input("Please enter the Month you were born: ")
+        myYear = input("Please enter the Year you were born: ")
 
-print("Your birthday is on "+str(myDay)+"-"+str(myMonth)+"-"+str(myYear))
+        print("Your birthday is on " + str(myDay) + "-" + str(myMonth) + "-" + str(myYear))
 
-# Calculating the user's age
-myBirthday = datetime.date(int(myYear), int(myMonth), int(myDay))
-today = datetime.date.today()
-dayDiff = (today-myBirthday)
+        # Calculating the user's age
+        myBirthday = datetime.date(int(myYear), int(myMonth), int(myDay))
+        today = datetime.date.today()
+        dayDiff = (today - myBirthday)
 
-# Print the user's age
-print("Your age is "+str(int(dayDiff.days/365.25))+" Years")
+        # Print the user's age
+        print("Your age is " + str(int(dayDiff.days / 365.25)) + " Years")
+        break;
+    except:
+        print("\nSorry, there is an error in your input!!")
